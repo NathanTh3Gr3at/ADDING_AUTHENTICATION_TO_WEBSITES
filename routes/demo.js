19057de800e1a3ea4144f62fa837e2540b8e036d
NathanTh3Gr3at <112683403+NathanTh3Gr3at@users.noreply.github.com>
2025-02-15 13:12:14 +0200

    first commit

---
+++ routes/demo.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const db = require("../data/database");
+
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.render("welcome");
+});
+
+router.get("/signup", function (req, res) {
+  res.render("signup");
+});
+
+router.get("/login", function (req, res) {
+  res.render("login");
+});
+
+router.post("/signup", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredConfirmEmail = userData["confirm-email"];
+  const enteredPassword = userData.password;
+
+  if (
+    !enteredEmail ||
+    !enteredConfirmEmail ||
+    !enteredPassword ||
+    enteredPassword.trim() < 6 ||
+    enteredEmail !== enteredConfirmEmail ||
+    !enteredEmail.includes("@")
+  ) {
+    return res.redirect('/signup')
+  }
+
+  const existingUser=await db.getDb().collection('users').findOne({email:enteredEmail})
+  if(existingUser){
+    return res.redirect('/signup')
+  }
+
+  const hashedPassword = await bcrypt.hash(enteredPassword, 12);
+
+  const user = {
+    email: enteredEmail,
+    password: hashedPassword,
+  };
+
+  const result = await db.getDb().collection("users").insertOne(user);
+  res.redirect("/login");
+});
+
+router.post("/login", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+
+  const enteredPassword = userData.password;
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+  if (!existingUser) {
+    return res.redirect("/login");
+  }
+  const passwordsAreEqual = await bcrypt.compare(
+    enteredPassword,
+    existingUser.password
+  );
+  if (!passwordsAreEqual) {
+    return res.redirect("/login");
+  }
+  res.redirect("/admin");
+});
+
+router.get("/admin", function (req, res) {
+  // check admin ticket
+  res.render("admin");
+});
+
+router.post("/logout", function (req, res) {});
+
+module.exports = router;
